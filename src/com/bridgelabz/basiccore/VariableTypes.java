d69517279d60e9aa2260610a93e87fee773cdeec
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-21 16:00:07 +0530

    Modify : Variable Types

--- src/com/bridgelabz/basiccore/VariableTypes.java
+++ src/com/bridgelabz/basiccore/VariableTypes.java
@@ -11,8 +11,14 @@
 cannot access out side the method or block
 if we use variable must initials
 
-*/
+2. Static Variable :- Static variables are allocated memory in data segment, not stack segment. See memory layout of C programs for details. 3) Static variables
 
+3. Instance variables :- The variables that are declared inside the class but outside the scope of any method are called instance variables
+Instance variables are non-static variables and are declared in a class outside of any method, constructor, or block.
+ Instance variables hold values that must be referenced by more than one method, constructor or block, or essential parts of an object's state
+
+*/
+// Variables Types
 package com.bridgelabz.basiccore;
 
 public class VariableTypes
