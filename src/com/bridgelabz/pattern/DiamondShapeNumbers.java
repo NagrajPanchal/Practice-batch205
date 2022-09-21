14439343ddd10b0ac2ee177e4f16338b9aa50877
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-21 09:25:29 +0530

    Refactor : Diamond Shape Numbers

---
+++ src/com/bridgelabz/pattern/DiamondShapeNumbers.java
@@ -0,0 +1,29 @@
+// Diamond Shape Numbers
+package com.bridgelabz.pattern;
+
+public class DiamondShapeNumbers
+{
+    public static void main(String[] args)
+    {
+        for (int i=0; i<5; i++)
+        {
+            for (int j=5; j>i+1; j--)
+            {
+                System.out.print(" ");
+            }
+            for (int j=0; j<=i*2; j++ )
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+        for(int i=5;i>0;i--)
+        {
+            for(int k=5;k>i;k--)
+                System.out.print(" ");
+            for(int j=0;j<i*2-1;j++)
+                System.out.print(j);
+            System.out.println();
+        }
+    }
+}
