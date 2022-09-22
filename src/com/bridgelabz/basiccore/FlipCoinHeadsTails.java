61beb06e2e80feab95e753495b9b8bafefc2c193
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-22 10:25:25 +0530

    Modify : Flip Coin Head and Tail Calculate Paersentage

--- src/com/bridgelabz/basiccore/FlipCoinHeadsTails.java
+++ src/com/bridgelabz/basiccore/FlipCoinHeadsTails.java
@@ -1,18 +1,60 @@
 //Flip coin toss Heads and Tails
 
 package com.bridgelabz.basiccore;
-
+import java.util.Scanner;
 public class FlipCoinHeadsTails
 {
     public static void main(String[] args)
     {
-        if (Math.random() < 0.5) //Math random function if less then 0.5 it will print Head or else Tail
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Flip the coin numbers of times : ");
+        int number = scanner.nextInt();
+        int totalCount=0,countHeads=0,countTails=0;
+        for(int i=0;i<number; i++)
         {
-            System.out.println("Heads");
+            if (Math.random() < 0.5) //Math random function if less then 0.5 it will print Head or else Tail
+            {
+                countHeads++;
+            } else
+            {
+                countTails++;
+            }
+            totalCount++;
         }
-        else
-        {
-            System.out.println("Tails");
-        }
+        System.out.println("Numbers of Heads: "+countHeads);
+        System.out.println("Numbers of Tails: "+countTails);
+        float Head = (float)((countHeads % totalCount) * 10);
+        float Tails = (float)((countTails % totalCount) * 10);
+        /*
+        int Head = ((countHeads / totalCount) * 100);
+        int Tails = ((countTails / totalCount) * 100);
+        */
+        /*
+        float Head = (float)((countHeads / totalCount) * 100);
+        float Tails = (float)((countTails / totalCount) * 100);
+        */
+        /*
+        double Head = (countHeads/totalCount)*100;
+        double Tails = (countTails/totalCount)*100; */
+        System.out.println("Percentage of Head : " +Head+"%");
+        System.out.println("Percentage of Tail : " +Tails+"%");
     }
 }
+
+/*
+Example::::::
+
+n Scanner function
+
+
+
+10  ===    0.23  0.45 0.78  0.98 0.75 0.47 0.21 0.85 0.569 0.54
+
+random 10 times
+
+less than 0.5  6
+above 0.5      4
+
+6/10 * 100 = 60%
+4/10 * 100 = 40%
+ */
