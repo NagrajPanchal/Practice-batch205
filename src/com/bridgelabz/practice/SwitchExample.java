2210494c7ceed8c46ee0f90a2aa45acd7b62d3e2
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-23 20:11:10 +0530

    Modify : Switch Case with Method

--- src/com/bridgelabz/practice/SwitchExample.java
+++ src/com/bridgelabz/practice/SwitchExample.java
@@ -22,7 +22,7 @@ public class SwitchExample
     }
     public static void main(String[] args)
     {
-        System.out.println("Main Method");
+        System.out.print("Enter A-Z Charter : ");
         Scanner scanner = new Scanner(System.in);
         char ch = scanner.next().charAt(0);
         method(ch);
