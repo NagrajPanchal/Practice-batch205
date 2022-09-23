00bafe83a205024bb666558bdf302bab9d9f2992
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-23 23:27:40 +0530

    Add : Power of 2 by using Recursive Function

--- src/com/bridgelabz/basiccore/powerbase/Power2RecursiveFunction.java
+++ src/com/bridgelabz/basiccore/powerbase/Power2RecursiveFunction.java
@@ -1,2 +1,18 @@
-package com.bridgelabz.basiccore.powerbase;public class Power2RecursiveFunction {
+// Power of two by using Recursive Function
+
+package com.bridgelabz.basiccore.powerbase;
+public class Power2RecursiveFunction
+{
+    public static void main (String[]args)
+    {
+        int base = 2, p=4;
+        System.out.println ("Required Power is " + power (base, p));
+    }
+    //Recursive Function
+    static int power (int base, int x)
+    {
+        if (x == 0)     //Base Condition
+            return 1;
+        return (base * power (base, x - 1));
+    }
 }
