373e91001f00abfd530a08dde56c7d911aaf850a
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-23 23:31:10 +0530

    Add : Power of 2 by using While loop

--- src/com/bridgelabz/basiccore/powerbase/PowerWhile.java
+++ src/com/bridgelabz/basiccore/powerbase/PowerWhile.java
@@ -1,2 +1,18 @@
-package com.bridgelabz.basiccore.powerbase;public class PowerWhile {
+// Power of 2 by using while Loop
+
+package com.bridgelabz.basiccore.powerbase;
+public class PowerWhile
+{
+    public static void main(String[] args) {
+        int num = 4, x = 2;
+        System.out.println("Required Power is " + power(x,num));
+    }
+    static int power(int num, int x) {
+        int result = 1;
+        while (x > 0) {
+            result *= num;
+            x--;
+        }
+        return result;
+    }
 }
