5d0657fbd357bbc2ee79b4539ab531cef1a4d4d5
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-23 19:33:57 +0530

    Add : Alaphabet Vowel Consonants

---
+++ src/com/bridgelabz/basiccore/AlphabetVowelConsonant.java
@@ -0,0 +1,23 @@
+// Check Whether an Alphabet is Vowel or Consonant
+
+package com.bridgelabz.basiccore;
+import java.util.Scanner;
+public class AlphabetVowelConsonant
+{
+    static void alphabet(char c)
+    {
+        switch (c)
+        {
+            case 'c':
+                System.out.println("Vowel");
+                break;
+            default:
+                System.out.println("Consonant");
+                break;
+        }
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
