143a422a34d207fa21e99dbdb7363ad5fe48b4c6
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-23 23:30:10 +0530

    Add : Product of Two Numbers

--- src/com/bridgelabz/basiccore/powerbase/PowerBase.java
+++ src/com/bridgelabz/basiccore/powerbase/PowerBase.java
@@ -1,2 +1,18 @@
-package com.bridgelabz.basiccore.powerbase;public class PowerBase {
+// calculates the power and base value
+
+package com.bridgelabz.basiccore.powerbase;
+import java.util.Scanner;
+public class PowerBase
+{
+    public static void main(String[] args) {
+
+        int base,power,result;
+        System.out.println("Enter the Base value :");
+        Scanner sc = new Scanner(System.in);
+        base = sc.nextInt();
+        System.out.println("Enter the Power value :");
+        power = sc.nextInt();
+        result = (int) Math.pow(base,power);
+        System.out.println("Results of Base^Power : " +base+ "^" +power+ " = " +result);
+    }
 }
