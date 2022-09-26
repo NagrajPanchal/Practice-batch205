4964ae6745bd164ecf509e0ac34829a7cfcb1aaf
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-09-26 21:47:35 +0530

    Modify : Diamond Shape Numbers

--- src/com/bridgelabz/pattern/DiamondShapeNumbers.java
+++ src/com/bridgelabz/pattern/DiamondShapeNumbers.java
@@ -5,27 +5,27 @@ public class DiamondShapeNumbers
 {
     public static void main(String[] args)
     {
-        for (int i=0; i<5; i++)
+        for (int i=1; i<5; i++)
         {
             for (int j=5; j>i+1; j--)
             {
                 System.out.print(" ");
             }
-            for (int j=0; j<=i*2; j++ )
+            for (int j=1; j<=i; j++ )
             {
-                System.out.print(j);
+                System.out.print(j+" ");
             }
             System.out.println();
         }
-        for(int i=5;i>0;i--)
+        for(int i=4;i>0;i--)
         {
-            for(int k=5;k>i;k--)
+            for(int k=4;k>i;k--)
             {
                 System.out.print(" ");
             }
-            for(int j=0;j<i*2-1;j++)
+            for(int j=1;j<i;j++)
             {
-                System.out.print(j);
+                System.out.print(" "+j);
             }
             System.out.println();
         }
