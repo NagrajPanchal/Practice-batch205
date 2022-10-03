e116ef51275acf77c5a91d494cdc7db7d509a5f0
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-03 23:49:19 +0530

    Refactor : Array Elements

--- src/com/bridgelabz/arrayhandling/ArrayElement.java
+++ src/com/bridgelabz/arrayhandling/ArrayElement.java
@@ -1,22 +1,17 @@
+// Array Elements
+
 package com.bridgelabz.arrayhandling;
-
-import java.util.Scanner;
-
 public class ArrayElement
 {
-
     public static void main(String[] args)
     {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Array Size : ");
-        int size = scanner.nextInt();
-        System.out.print("Enter Array Size : ");
-        int[] arr = new int[size];
+        System.out.println("Array Array Elements are : ");
+        int[] arr = new int[8];
         arr[0] = 10;
         arr[2] = 20;
         arr[4] = 22;
         arr[6] = 34;
-        for(int i=0; i< size; i++)
+        for(int i=0; i< arr.length; i++)
         {
             System.out.println(arr[i]);
         }
