54057011dde2223bc4b9a2d7a306f236812d8f69
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-04 01:27:17 +0530

    Added : Arry of Element in-Line Array

---
+++ src/com/bridgelabz/arrayhandling/ArrayOfElementsInLine.java
@@ -0,0 +1,9 @@
+// Array of Elements for each loop in line Array
+package com.bridgelabz.arrayhandling;
+public class ArrayOfElementsInLine {
+    public static void main(String[] args) {
+        int[] arr = {10, 22, 43, 45, 56, 1, 35, 33, 3, 78};
+        for (int b : arr)
+            System.out.print(b+" ");
+    }
+}
