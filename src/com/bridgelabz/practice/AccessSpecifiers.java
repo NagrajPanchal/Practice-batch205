5ee173d83301a3ad81ade16c659f8fe7100cf361
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-04 17:29:26 +0530

    Updated : Public Private Protected

--- src/com/bridgelabz/practice/AccessSpecifiers.java
+++ src/com/bridgelabz/practice/AccessSpecifiers.java
@@ -6,8 +6,25 @@ package com.bridgelabz.practice;
 public class AccessSpecifiers
 {
     static private int privateVar;
+    static private int getPrivateVar(int a)
+    {
+        a = 10;
+        return a;
+    }
+    public static void getPublicVar()
+    {
+        int a = 10;
+        System.out.println(a);
+    }
+    protected static void getProtectedVar()
+    {
+        int a = 10;
+        System.out.println("Protected method : "+a);
+    }
     public static void main(String[] args)
     {
-
+        System.out.println(privateVar); //Private variable
+        System.out.println(AccessSpecifiers.getPrivateVar(10)); // Private method
+        getPublicVar();
     }
 }
