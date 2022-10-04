275a238a5839c42e5f06d7adeee1ee5d0d5bcb6c
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-04 15:35:38 +0530

    Modify : Smallest Array of element

--- src/com/bridgelabz/arrayhandling/SmallestArrayElement.java
+++ src/com/bridgelabz/arrayhandling/SmallestArrayElement.java
@@ -1,2 +1,33 @@
-package com.bridgelabz.arrayhandling;public class SmallestArrayElement {
+// Smallest Array of Number
+
+package com.bridgelabz.arrayhandling;
+public class SmallestArrayElement
+{
+    void smallestNumber()
+    {
+        int[] arr = {11,20,30,40,50,60};
+//        int[] arr =new int[10];
+//        arr[0] = 10;
+//        arr[1] = 20;
+//        arr[3] = 10;
+//        arr[4] = 40;
+//        arr[5] = 50;
+//        arr[6] = 80;
+//        arr[8] = 23;
+//        arr[9] = 25;
+        int min = arr[0];
+        for(int i = 0; i< arr.length; i++)
+        {
+            if(arr[i] < min)
+            {
+                min = arr[i];
+            }
+        }
+        System.out.println(min + " is smallest Array of Element");
+    }
+    public static void main(String[] args)
+    {
+        SmallestArrayElement small = new SmallestArrayElement();
+        small.smallestNumber();
+    }
 }
