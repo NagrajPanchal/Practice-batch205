09e34afdccad6bd00faee6d20b20419d5dac4052
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-04 14:50:14 +0530

    Modify : Largest arry of element

--- src/com/bridgelabz/arrayhandling/LargestElementOfArray.java
+++ src/com/bridgelabz/arrayhandling/LargestElementOfArray.java
@@ -1,2 +1,25 @@
-package com.bridgelabz.arrayhandling;public class LargestElementOfArray {
+// Largest Element in an Array
+
+package com.bridgelabz.arrayhandling;
+
+public class LargestElementOfArray
+{
+    static boolean largestNumber;
+    public static void largest()
+    {
+        int[] arr = {12,13,10,25,40,51};
+        int max = arr[0];
+        for(int i=0; i< arr.length;i++)
+        {
+                if (arr[i] > max)
+                {
+                   max = arr[i];
+                }
+        }
+        System.out.println(max+" is Largest Array of Element");
+    }
+    public static void main(String[] args)
+    {
+        largest();
+    }
 }
