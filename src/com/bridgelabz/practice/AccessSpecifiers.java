91995afe85dcf45d7e42d9ff9f5f60de76210972
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-04 14:51:36 +0530

    Added : Access Specifiers

--- src/com/bridgelabz/practice/AccessSpecifiers.java
+++ src/com/bridgelabz/practice/AccessSpecifiers.java
@@ -1,2 +1,13 @@
-package com.bridgelabz.practice;public class AccessSpecifiers {
+// Access Specifiers
+// public,private,protected,default
+
+package com.bridgelabz.practice;
+
+public class AccessSpecifiers
+{
+    static private int privateVar;
+    public static void main(String[] args)
+    {
+
+    }
 }
