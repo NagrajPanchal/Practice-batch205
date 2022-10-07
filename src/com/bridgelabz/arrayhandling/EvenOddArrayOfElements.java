b03b7763b4c1259bb7a1b8639e396553d6973bc9
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-08 00:41:49 +0530

    Added :  Even and Odd Array Elements

--- src/com/bridgelabz/arrayhandling/EvenOddArrayOfElements.java
+++ src/com/bridgelabz/arrayhandling/EvenOddArrayOfElements.java
@@ -1,2 +1,40 @@
-package com.bridgelabz.arrayhandling;public class EvenOddArrayOfElements {
+// Even and Odd Array of Elements
+
+package com.bridgelabz.arrayhandling;
+
+public class EvenOddArrayOfElements
+{
+    public static int evenSize = 0;
+    public static int oddSize = 0;
+    public static int[] even = new int[evenSize];
+    public static int[] odd = new int[oddSize];
+    public static int j,k;
+    public static void evenOdd()
+    {
+        for(int i =0; i <DuplicateArrayOfElement.arr.length; i++)
+        {
+            if(DuplicateArrayOfElement.arr[i] % 2 == 0)
+                evenSize++;
+            else
+                oddSize++;
+        }
+        for(int i =0; i <DuplicateArrayOfElement.arr.length; i++)
+        {
+            if(DuplicateArrayOfElement.arr[i] % 2 == 0)
+                even[j++] = DuplicateArrayOfElement.arr[i];
+            else
+                odd[k++] = DuplicateArrayOfElement.arr[i];
+        }
+        System.out.print("Even Array Elements : ");
+        System.out.println(even);
+        System.out.print("Odd Array Elements : ");
+        System.out.println(odd);
+    }
+    public static void main(String[] args)
+    {
+        DuplicateArrayOfElement.userDefinedArrayOfSizeAndElements();
+        DuplicateArrayOfElement.arrayOfElements();
+        evenOdd();
+
+    }
 }
