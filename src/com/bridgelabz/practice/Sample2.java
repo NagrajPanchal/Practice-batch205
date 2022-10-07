8a3407fa4308dc7b60d13f5cf55b8cd6eca12f57
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-07 22:44:33 +0530

    Added : Static method outside the class

--- src/com/bridgelabz/practice/Sample2.java
+++ src/com/bridgelabz/practice/Sample2.java
@@ -4,6 +4,9 @@ public class Sample2
 {
     public static void main(String[] args)
     {
-        System.out.println("Static Variable types of calss " + VariableTypes.staticVar1);
+        System.out.println("Static Variable types of class " + VariableTypes.staticVar1);
+        //Accessing Non-Static method outside the Class
+        MethodTypes obj =new MethodTypes(); // object created for outside the class
+        obj.m2(); // Outside the class created the object reference access non-static method
     }
 }
