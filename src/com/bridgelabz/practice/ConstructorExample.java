6cba067b69ec7770e9542bc1913246226d9a42d1
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-07 22:42:05 +0530

    Added : Constructor class

--- src/com/bridgelabz/practice/ConstructorExample.java
+++ src/com/bridgelabz/practice/ConstructorExample.java
@@ -1,2 +1,29 @@
-package com.bridgelabz.practice;public class ConstructorExample {
+// Constructor
+// Types of Constructor
+// Default Constructor or no-argument Constructor
+// Parameterized Constructor
+// Copy Constructor
+// Static Constructor
+// Private Constructor
+
+// Default Constructors: Default constructor is the constructor which doesn't take any argument. It has no parameters.
+
+package com.bridgelabz.practice;
+import com.bridgelabz.oops.Dog;
+public class ConstructorExample
+{
+    // Constructor => It is a special method having same name as Class name
+    // It is Used to Initialize an object.
+    // Compiler create a default  constructor when there is no constructor
+    ConstructorExample()
+    {
+        System.out.println("I'm Constructor");
+
+    }
+    public static void main(String[] args)
+    {
+        ConstructorExample obj = new ConstructorExample();
+        Dog dog1 = new Dog(); // Instance Variable will give a memory
+        System.out.println(dog1.VOICE);
+    }
 }
