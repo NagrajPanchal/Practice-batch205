da900fc69b2548a856089241b3578a8c817b7183
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-08 00:42:17 +0530

    idea

--- src/com/bridgelabz/arrayhandling/DuplicateArrayOfElement.java
+++ src/com/bridgelabz/arrayhandling/DuplicateArrayOfElement.java
@@ -4,36 +4,44 @@ package com.bridgelabz.arrayhandling;
 import java.util.Scanner;
 public class DuplicateArrayOfElement
 {
+    public static int size;
+    public static int[] arr;
     public static void userDefinedArrayOfSizeAndElements()
     {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter Size of Array : ");
-        int size = scanner.nextInt();
-        int[] arr = new int[size];
+        size = scanner.nextInt();
+        arr = new int[size];
         System.out.print("Enter Array of Elements : ");
         for (int i=0; i<arr.length;i++)
         {
             arr[i] = scanner.nextInt();
         }
+    }
+    public static void arrayOfElements()
+    {
         System.out.print("Array of Elements : ");
         for (int b : arr)
             System.out.print(b+" ");
     }
     public static void duplicateElements()
     {
-        userDefinedArrayOfSizeAndElements();
-        System.out.println("Duplicate array of Elements : ");
-        int arr;
-//        for(int i = 0; i<arr.length; i++)
-//        {
-//            for(int j = i+1; j<arr.length; j++)
-//            {
-//                System.out.print(arr[j]);
-//            }
-//        }
+        System.out.print("\nDuplicate array of Elements : ");
+        for(int i = 0; i<arr.length-1; i++)
+        {
+            for(int j = i+1; j<arr.length; j++)
+            {
+                if(arr[i] == arr[j])
+                {
+                    System.out.println(arr[j]);
+                }
+            }
+        }
     }
     public static void main(String[] args)
     {
+        userDefinedArrayOfSizeAndElements();
+        arrayOfElements();
         duplicateElements();
 
     }
