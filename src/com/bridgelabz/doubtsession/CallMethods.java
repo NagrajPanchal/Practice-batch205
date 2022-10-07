43087b3a65de52279608f49b3205288a69e2db84
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-07 22:37:53 +0530

    Added : Call Methods Static and Non-static

--- src/com/bridgelabz/doubtsession/CallMethods.java
+++ src/com/bridgelabz/doubtsession/CallMethods.java
@@ -1,2 +1,12 @@
-package com.bridgelabz.doubtsession;public class CallMethods {
+// Static and Non-Static methods
+
+package com.bridgelabz.doubtsession; // Package
+public class CallMethods // Class
+{
+    public static void main(String[] args) // Main Method
+    {
+        StaticAndNonStaticMethod.staticMethod(); // Static Method : Outside the class by using class name
+        StaticAndNonStaticMethod non = new StaticAndNonStaticMethod(); // Object Created
+        non.nonStaticMethod(); // Non-Static Method : Outside the class create object by using object reference
+    }
 }
