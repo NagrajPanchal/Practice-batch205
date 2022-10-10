b91f142f41655d5af7721e02aef45c8317d4d95f
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-10 23:54:45 +0530

    Static and Non-static method

--- src/com/bridgelabz/practice/MethodTypes2.java
+++ src/com/bridgelabz/practice/MethodTypes2.java
@@ -1,2 +1,34 @@
-package com.bridgelabz.practice;public class MethodTypes2 {
+package com.bridgelabz.practice;
+
+public class MethodTypes2
+{
+    public static int staticVar;
+    int instanceVar;
+    static void method1()
+    {
+        System.out.println("static Method");
+        int staticVar = 10;
+        System.out.println(staticVar);
+        MethodTypes2 mt2 = new MethodTypes2();
+        System.out.println(mt2.staticVar);
+        System.out.println(mt2.instanceVar);
+        System.out.println(MethodTypes2.staticVar);
+        System.out.println();
+    }
+    void method2(int instanceVar)
+    {
+        System.out.println("Non-static Method");
+        int staticVar = 20;
+        System.out.println(staticVar);
+        System.out.println(this.instanceVar);
+        System.out.println(MethodTypes2.staticVar);
+        MethodTypes2 mt4 = new MethodTypes2();
+        System.out.println(mt4.instanceVar);
+    }
+    public static void main(String[] args)
+    {
+        method1();
+        MethodTypes2 mt3 = new MethodTypes2();
+        mt3.method2(12);
+    }
 }
