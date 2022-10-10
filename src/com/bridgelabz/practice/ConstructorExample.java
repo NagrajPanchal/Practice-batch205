750b793b56c537f3f0518e8772e08700b5c49176
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-10 23:54:13 +0530

    Constructor

--- src/com/bridgelabz/practice/ConstructorExample.java
+++ src/com/bridgelabz/practice/ConstructorExample.java
@@ -8,6 +8,11 @@
 
 // Default Constructors: Default constructor is the constructor which doesn't take any argument. It has no parameters.
 
+// Constructor => It is a special method having same name as class name
+// It is used to initialize an object.
+// Compiler creates a default constructor when there is no constructor present in the class.
+// When the constructor is already created , then compiler will not create any constructor.
+
 package com.bridgelabz.practice;
 import com.bridgelabz.oops.Dog;
 public class ConstructorExample
@@ -20,10 +25,16 @@ public class ConstructorExample
         System.out.println("I'm Constructor");
 
     }
+
     public static void main(String[] args)
     {
         ConstructorExample obj = new ConstructorExample();
-        Dog dog1 = new Dog(); // Instance Variable will give a memory
-        System.out.println(dog1.VOICE);
+//        Dog dog1 = new Dog(); // Instance Variable will give a memory
+//        System.out.println(); // null
+//        System.out.println(dog1.age); // 0
+//        System.out.println(dog1.color); // null
+//        System.out.println(dog1.VOICE);
+//        dog1.VOICE = "Meow";
+//        System.out.println(dog1.VOICE);
     }
 }
