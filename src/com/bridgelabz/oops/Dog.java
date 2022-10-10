8af279dbd509bd852c526d036a5b8b11f7982348
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-10 23:50:15 +0530

    Constructor, Methods thyes

--- src/com/bridgelabz/oops/Dog.java
+++ src/com/bridgelabz/oops/Dog.java
@@ -3,11 +3,46 @@ package com.bridgelabz.oops;
 
 public class Dog
 {
-    // When the Constructor is already created then complier will not create
+    int height,weight,length;
+
+    // Compiler creates a default constructor when there is no constructor present in the class.
+    // When the constructor is already created , then compiler will not create any constructor.
     public Dog()
     {
 
     }
+    public Dog(int dogOfAge)
+    {
+        age = dogOfAge;
+    }
+    public Dog(String breedOfDog, int ageOFDog)
+    {
+     breed = breedOfDog;
+     age = ageOFDog;
+    }
+    // this keyword is used for refering to the current object
+    public Dog(String breed,int age,String color)
+    {
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+    public Dog(int heightOfDog, int weightOfDog, int lengthOfDog)
+    {
+        this.height = heightOfDog;
+        this.weight = weightOfDog;
+        this.length = lengthOfDog;
+    }
+    //    public Dog(String breedOfDog,int ageOfDog,String colorOfDog)
+//    {
+//        breed = breedOfDog;
+//        age = ageOfDog;
+//        color = colorOfDog;
+//    }
+
+
+
+
     static String breed; // null
     static int age; // 0
     static String color; // null
@@ -70,9 +105,24 @@ public class Dog
         System.out.println(dog2.color);
         System.out.println(dog2.VOICE);
         System.out.println(dog1.breed+VOICE+x);
+        System.out.println(dog1.age+100);
 
         System.out.println(dog1.age);
+        Dog dog11 = new Dog(11);
+        System.out.println(dog11.age);
+        System.out.println(dog11.age+age);
+        Dog dog12 = new Dog("Local",12);
+        System.out.println(dog12.breed);
+        System.out.println(dog12.age);
+        Dog dog3 = new Dog("Pitbull",13,"White");
+        System.out.println(dog3.breed);
+        System.out.println(dog3.age);
+        System.out.println(dog3.color);
 
+        Dog dog14 = new Dog(4,15,3);
+        System.out.println(dog14.height);
+        System.out.println(dog14.weight);
+        System.out.println(dog14.length);
     }
 
 }
