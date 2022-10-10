7ac3bf348190292febd7cf46337aee239f98bbc7
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-10 23:53:21 +0530

    Setter and getter method

--- src/com/bridgelabz/oops/Person.java
+++ src/com/bridgelabz/oops/Person.java
@@ -1,2 +1,42 @@
-package com.bridgelabz.oops;public class Person {
+package com.bridgelabz.oops;
+
+public class Person
+{
+    private String name;
+    private int age;
+    private String  gender;
+    private String adhaarNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAdhaarNumber() {
+        return adhaarNumber;
+    }
+
+    public void setAdhaarNumber(String adhaarNumber) {
+        this.adhaarNumber = adhaarNumber;
+    }
+
 }
