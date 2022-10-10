59d0722d42823739c451d55ae6089bff867570fd
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-10 23:52:43 +0530

    OOPS Concepts

--- src/com/bridgelabz/oops/OopsBasic.java
+++ src/com/bridgelabz/oops/OopsBasic.java
@@ -1,2 +1,18 @@
-package com.bridgelabz.oops;public class OopsBasic {
+package com.bridgelabz.oops;
+
+// OOPS :-> Object Oriented Programming Systems
+// OOPS features :-> Encapsulation, Inheritance, Abstraction, Polymorphism
+// Association :-> Composition and Aggregation.
+// Constructor : -> super,this,super(), this()
+
+// Encapsulation :-> Binding a data in single unit
+// We achieve data hiding through encapsulation
+
+public class OopsBasic
+{
+
+    public static void main(String[] args)
+    {
+
+    }
 }
