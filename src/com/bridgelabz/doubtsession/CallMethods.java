3254ba02864eb137d1c44d1b60426601a95be62a
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-11 22:27:40 +0530

    Refactor : call methods & variables

--- src/com/bridgelabz/doubtsession/CallMethods.java
+++ src/com/bridgelabz/doubtsession/CallMethods.java
@@ -1,6 +1,9 @@
 // Static and Non-Static methods
 
 package com.bridgelabz.doubtsession; // Package
+
+import com.bridgelabz.oops.Student;
+
 public class CallMethods // Class
 {
     public static void main(String[] args) // Main Method
@@ -8,5 +11,17 @@ public class CallMethods // Class
         StaticAndNonStaticMethod.staticMethod(); // Static Method : Outside the class by using class name
         StaticAndNonStaticMethod non = new StaticAndNonStaticMethod(); // Object Created
         non.nonStaticMethod(); // Non-Static Method : Outside the class create object by using object reference
+        Student objStudent = new Student();
+        objStudent.setStudentID(01);
+        objStudent.setRollNumber(143);
+        objStudent.setDepartment("CSE");
+        objStudent.setCollageName("GNDECB");
+        System.out.println(objStudent.getStudentID());
+        System.out.println(objStudent.getRollNumber());
+        System.out.println(objStudent.getDepartment());
+        System.out.println(objStudent.getCollageName()); // Private setter and Getter method
+        System.out.println(objStudent.age); // Public
+//        System.out.println(objStudent.cityMethod());// Protected variable diferent class by method
+
     }
 }
