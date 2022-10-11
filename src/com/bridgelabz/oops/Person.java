3a336d1a16a1fe7a3cd25837691dcd6bd834633c
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-11 22:20:34 +0530

    Refactor : Personn details added

--- src/com/bridgelabz/oops/Person.java
+++ src/com/bridgelabz/oops/Person.java
@@ -2,10 +2,21 @@ package com.bridgelabz.oops;
 
 public class Person
 {
+    // Encapsulation
     private String name;
     private int age;
     private String  gender;
     private String adhaarNumber;
+
+    public Person() {
+    }
+
+    public Person(String name, int age, String gender, String adhaarNumber) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.adhaarNumber = adhaarNumber;
+    }
 
     public String getName() {
         return name;
@@ -37,6 +48,11 @@ public class Person
 
     public void setAdhaarNumber(String adhaarNumber) {
         this.adhaarNumber = adhaarNumber;
+    }
+
+    void EmployeeDetailsConstructor()
+    {
+        new Person("Nagraj",29,"Male","1234-3454-2354");
     }
 
 }
