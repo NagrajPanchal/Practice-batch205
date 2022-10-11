dbb71b375898232359e298e030e0d9ab1e1d63d6
Nagraj <nagrajsubhashpanchal.gmail.com>
2022-10-11 22:21:59 +0530

    Refactor : Sample3 access classes private variables by using setter ans getter method

--- src/com/bridgelabz/oops/Sample3.java
+++ src/com/bridgelabz/oops/Sample3.java
@@ -6,6 +6,9 @@ public class Sample3
 {
     public static void main(String[] args)
     {
+        System.out.println("***************************");
+        System.out.println("Person class accessing by object via setter and getter method");
+        System.out.println("Private variable outside the class by using setter and getter method");
             Person per = new Person();
             per.setName("Nagraj");
             per.setAge(20);
@@ -25,6 +28,48 @@ public class Sample3
         System.out.println(obj.getAge());
         System.out.println(obj.getGender());
         System.out.println(obj.getAdhaarNumber());
+
+        System.out.println("***************************");
+        System.out.println("Parent class inherited in student class and access through setter and getter method ");
+        Student st = new Student(); // Object created for default constructor
+        st.setStudentID(111);
+        st.setName("Panchal");
+        st.setCollageName("GPT");
+        System.out.println(st.getStudentID()); // access Student private variable by using setter and getter method
+        System.out.println(st.getName()); // access Parent class inherited in student class
+        System.out.println(st.getCollageName());
+        System.out.println();
+        System.out.println("access non static method but private variables");
+        st.displayStudentDetails();
+
+        System.out.println("***************************");
+        System.out.println("Access through parametrized constructor");
+        Student stobj = new Student(706,72,"POLICE","BMSI"); // Parametrized constructor object created
+        System.out.println(stobj.getStudentID()); // call parametrized constructor object
+        System.out.println(stobj.getRollNumber());
+        System.out.println(stobj.getDepartment());
+        System.out.println(stobj.getCollageName());
+        stobj.cityMethod();// access non-static method inside parametrized constructor
+
+
+
+        System.out.println("***************************");
+        System.out.println("Access Employee details");
+        System.out.println("Constructor Object created access private variable via getter and setter method");
+        Employee emp = new Employee();
+        emp.setEmployeeID(16);
+        emp.setDepartment("SDE");
+        emp.setSalary(54000);
+        emp.setName("Nagraj");
+        System.out.println(emp.getName()); // inherited getter method from Person class in Employee class
+        System.out.println(emp.getEmployeeID()); // call getter method from employee class
+        System.out.println(emp.getDepartment());
+        System.out.println(emp.getSalary());
+
+        System.out.println("*****************************");
+        System.out.println("access non-static method by using constructor object");
+        emp.displayEmployeeDetails();
+
 
     }
 }
